3c9069b876bdb8ab5aa061aa32db046be6845662
luljn <mbohlulajonathan4@gmail.com>
2024-06-07 23:07:58 +0200

    Templates added

--- run_server.py
+++ run_server.py
@@ -6,23 +6,27 @@ Created on May 2022
 
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 # définir le message secret
 SECRET_MESSAGE = "Azertyuiop_1234!#qprtt"
 app = Flask(__name__)
 
-
-@app.route("/")
 def get_secret_message():
     return SECRET_MESSAGE
 
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/home")
+def getHome():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     # HTTP version
     # app.run(debug=True, host="0.0.0.0", port=8081)
     
     # HTTPS version
-    # A compléter  : nécessité de déplacer les bons fichiers vers ce répertoire
     context = ("resources/server-public-key.pem", "resources/server-private-key.pem")
-    app.run(ssl_context = context, port=8081)
+    app.run(ssl_context = context, port = 8081)
